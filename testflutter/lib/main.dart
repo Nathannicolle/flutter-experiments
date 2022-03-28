873bc65aa62f49a01752a8c0288ebe190dfac781
Nathannicolle <nathan-nicolle@orange.fr>
2022-03-28 09:58:41 +0200

    adding list

--- testflutter/lib/main.dart
+++ testflutter/lib/main.dart
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<String> todoItems = <String>['Item 1', 'Item 2', 'Item 3'];
 
   void _incrementCounter() {
     setState(() {
@@ -81,36 +82,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
-        ),
+        body: ListView.builder(
+            itemCount: todoItems.length,
+            itemBuilder: (context, index) {
+              return Container(
+                child: Text('${todoItems[index]}'),
+              );
+            }),
+
         floatingActionButton: Stack(children: <Widget>[
           Align(
               alignment: Alignment.bottomLeft,
